88257fb3123ba2cfef3413114f79ddc90784ff11
nsobotka <90986529+nsobotka@users.noreply.github.com>
2023-03-27 16:02:36 -0400

    Working emojis

---
+++ index.js
@@ -0,0 +1,26 @@
+
+window.onload = function onEmojiChange() {
+    var e = document.getElementById('reaction');
+    var text = e.options[e.selectedIndex].text;
+    document.getElementById('emoji').innerHTML = text;
+};
+function onEmojiChange() {
+    var e = document.getElementById('reaction');
+    var text = e.options[e.selectedIndex].text;
+    document.getElementById('emoji').innerHTML = text;
+};
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    var nodeCopy = document.getElementById(data).cloneNode(true);
+    ev.target.innerHTML = nodeCopy.innerHTML;
+}
