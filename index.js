b90df3908ad92a38184137346b2118d2d610fbec
nsobotka <90986529+nsobotka@users.noreply.github.com>
2023-03-28 23:22:53 -0400

    Working chat feature

--- index.js
+++ index.js
@@ -27,3 +27,59 @@ function drop(ev) {
     var nodeCopy = document.getElementById(data).cloneNode(true);
     ev.target.innerHTML = nodeCopy.innerHTML;
 }
+
+function toggleDiv(id) {
+    var div = document.getElementById(id);
+    // console.log(div.style);
+    div.style.display = div.style.display == "none" ? "block" : "none";
+}
+
+function init() {
+    var chatEnter = document.getElementById("freeform");
+    // Execute a function when the user presses a key on the keyboard
+    chatEnter.addEventListener("keypress", function (event) {
+        // If the user presses the "Enter" key on the keyboard
+        if (event.key === "Enter") {
+            // Cancel the default action, if needed
+            console.log("Hit enter");
+            event.preventDefault();
+            // Trigger the button element with a click
+            const newDiv0 = document.createElement("div");
+            var newContent = document.createTextNode("Me:");
+            newDiv0.style.position = "relative";
+            newDiv0.style.marginTop = "20px";
+            newDiv0.style.left = "10px";
+            newDiv0.style.width = "30px";
+            newDiv0.appendChild(newContent);
+            document.getElementById("innerChat").appendChild(newDiv0);
+
+            const newDiv = document.createElement("div");
+            var newContent = document.createTextNode(document.getElementById("freeform").value);
+            document.getElementById("freeform").value="";
+            newDiv.style.position = "relative";
+            newDiv.style.top = "0px";
+            newDiv.style.padding = "10px";
+            newDiv.style.left = "100px";
+            newDiv.style.width = "350px";
+            newDiv.style.fontSize = "20px";
+            newDiv.style.borderStyle = "solid";
+            newDiv.style.borderWidth = "1px";
+            newDiv.style.borderColor = "black";
+            newDiv.style.borderRadius = "15px";
+            newDiv.style.marginTop = "-30px";
+            newDiv.appendChild(newContent);
+            document.getElementById("innerChat").appendChild(newDiv);
+            var objDiv = document.getElementById("chatbox");
+            objDiv.scrollTop = objDiv.scrollHeight;
+        }
+    });
+}
+
+document.addEventListener('readystatechange', function () {
+    if (document.readyState === "complete") {
+        init();
+    }
+});
+
+
+
