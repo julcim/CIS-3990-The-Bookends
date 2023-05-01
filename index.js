3adb2645a0f9f1c350e11d63ae83264c5d4c4d03
julcim <juliannacimillo@gmail.com>
2023-04-30 22:14:36 -0400

    first try at websocket

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function changeCursor(event) {
             value = true;
             selected = 5;
         }
-        
+
         if (isEndCoordsSet == true) {
             document.getElementById("main_body").style.cursor = "auto";
             isEndCoordsSet = false;
@@ -85,7 +85,7 @@ function changeCursor(event) {
             document.body.appendChild(newDiv);
 
             selected = -1;
-        }  else if (isEndCoordsSet) {
+        } else if (isEndCoordsSet) {
             isEndCoordsSet = false;
         }
     } else if (value == true && (event.target === document.getElementById("Text1") || event.target === document.getElementById("Text2"))) {
@@ -206,7 +206,7 @@ function getLocationOfMouseUp(callback) {
             const rect = range.getBoundingClientRect(); // Get the bounding rect of the range
             const cursorRect = selection.getRangeAt(0).getClientRects()[0]; // Get the rect of the cursor at the end of the range
             endCoords.x = event.clientX - 20; // X coordinate of the cursor at the end of the event
-            endCoords.y = event.clientY - 20; 
+            endCoords.y = event.clientY - 20;
             isEndCoordsSet = true; // Set the global boolean flag to true
             callback(highlightedText); // Call the callback function with highlighted text
         }
@@ -223,5 +223,69 @@ getLocationOfMouseUp(function (highlightedText) {
 
 
 
+// Establish WebSocket connection
+const socket = new WebSocket('localhost:8080');
 
+// When WebSocket connection is opened
+socket.addEventListener('open', (event) => {
+    console.log('WebSocket connection opened:', event);
+});
+
+// When WebSocket connection receives a message
+socket.addEventListener('message', (event) => {
+    const data = JSON.parse(event.data);
+
+    // Append new message to chatbox
+    const newDiv0 = document.createElement("div");
+    var newContent = document.createTextNode("Me:");
+    newDiv0.style.position = "relative";
+    newDiv0.style.marginTop = "20px";
+    newDiv0.style.left = "10px";
+    newDiv0.style.width = "30px";
+    newDiv0.appendChild(newContent);
+    document.getElementById("innerChat").appendChild(newDiv0);
+
+    const newDiv = document.createElement("div");
+    var newContent = document.createTextNode(data);
+    document.getElementById("freeform").value = "";
+    newDiv.style.position = "relative";
+    newDiv.style.top = "0px";
+    newDiv.style.padding = "10px";
+    newDiv.style.left = "100px";
+    newDiv.style.width = "330px";
+    newDiv.style.fontSize = "20px";
+    newDiv.style.borderStyle = "solid";
+    newDiv.style.borderWidth = "1px";
+    newDiv.style.borderColor = "black";
+    newDiv.style.borderRadius = "15px";
+    newDiv.style.marginTop = "-30px";
+    newDiv.appendChild(newContent);
+    document.getElementById("innerChat").appendChild(newDiv);
+    var objDiv = document.getElementById("chatbox");
+    objDiv.scrollTop = objDiv.scrollHeight;
+});
+
+// When form is submitted
+const form = document.querySelector('.textbox form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // Get user's message
+    const textarea = document.getElementById('freeform');
+    const message = textarea.value.trim();
+
+    // If message is not empty
+    if (message) {
+        print("reached");
+        // Send message over WebSocket connection
+        const data = {
+            name: 'Me',
+            message: message
+        };
+        socket.send(JSON.stringify(data));
+
+        // Clear textarea
+        textarea.value = '';
+    }
+});
 
